8e3c16a83ed257cdcb1dbf5104244abb7c0eaad9
evgeniy-kulikov <redstepway@gmail.com>
2023-01-09 13:17:13 +0300

    start

---
+++ solutions.py
@@ -0,0 +1,15 @@
+# ввод данных. Данные вводятся через пробел, одной строкой
+# n, k = map(int, input().split())
+
+"""
+тернарный оператор:
+
+[when_true] if [condition] else [when_false]
+is_fast = True
+car = "Ferrari" if is_fast else "Sedan"
+
+тернарный оператор с кортежем:
+(when_false, when_true)[condition]
+is_fast = True
+car = ("Sedan", "Ferrari")[is_fast]
+"""
