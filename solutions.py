5dfda0b053f873167a313ef8615180b4741f2203
evgeniy-kulikov <redstepway@gmail.com>
2023-03-25 12:57:48 +0300

    6.6 Генераторы множеств и словарей

--- solutions.py
+++ solutions.py
@@ -17,3 +17,16 @@ car = ("Sedan", "Ferrari")[is_fast]
 lst = ["a", "b", "c"]
 ''.join(map(str, lst))  # способ преобразовать список в строку
 "abc"
+
+
+# реализация функции enumerate()
+def get_even(list_of_nums):
+    for el in list_of_nums:
+        if el % 2 == 0:
+            yield el
+
+
+list_of_nums = [1, 2, 3, 8, 15, 42]
+# вывод только четных значений из списка
+for item in get_even(list_of_nums):
+    print(item, end=" ")  # 2 8 42
